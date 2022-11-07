e30fb06e7f0b72670211cda34e71dfb59ff833ef
safaan0313 <safaanahamedh313@gmail.com>
2022-11-07 16:11:32 +0530

    files

---
+++ ifelse.java
@@ -0,0 +1,30 @@
+import java.util.scanner;
+Public class ifelse{
+
+
+	public static void main (String args[]) {
+
+       String s="Hello"; s="How are you??";
+		void Method1()
+
+		{ System.out.println("how is it going");}
+
+
+		void Method2()
+
+		{ System.out.println("how are you??"); }
+
+
+		scanner s = new scanner(System.in);
+
+
+		if(s=Hello)
+		{ Method1();}
+
+                elseif(s=How are you??)
+		{ Method2();}
+
+		else()
+		{System.out.println("say something!!!");}
+
+	}}
